f1359a34927e321e9f199aac1df76fb6c1b70700
hossem007 <86675005+hossem007@users.noreply.github.com>
2021-08-03 16:02:51 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function sumArr(arrA, arrB) {
+    var sum = 0;
+    var sumB = 0;
+    var CommonElement='';
+    for (var i = 0; i < arrA.length; i++) {
+        var equal = false;
+        for (var j = 0; j < arrB.length; j++) {
+            if (arrB[j] === arrA[i]) {
+                equal = true;
+                CommonElement+=arrA[j]+' ';
+                //break;
+            }
+        }
+        if (equal === false) {
+            sum += arrA[i];
+        }
+
+    }
+    for (var i = 0; i < arrB.length; i++) {
+        for (var j = 0; j < arrA.length; j++) {
+            if (arrA[j] === arrB[i]) {
+                //break;
+            }
+        }
+        if (equal === false) {
+            sumB += arrB[i];
+        }
+
+    }
+    console.log('Sum of overlapping elements: ',sum+sumB,'\n');
+    console.log('Explanation: Common elements are: ',CommonElement);
+}
+
+console.log(sumArr([12, 13, 6, 10],[13, 10, 16, 15]));
